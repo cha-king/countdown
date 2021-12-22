6ab00891bbc1db62e3fc504460fa12869126851f
Charlie King <charlie.king@verkada.com>
2021-12-22 13:05:28 -0800

    Working calendar API

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 const { URL, URLSearchParams } = require('url');
 const { randomBytes } = require('crypto');
-const axios = require('axios');
+const axios = require('axios').default;
 
 const CLIENT_ID = '339864928771-mtbpret2idljjjlsvto3h4uoglbfi0u9.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'http://127.0.0.1'
+const SCOPES = [
+    'https://www.googleapis.com/auth/calendar.events.readonly',
+];
+const CALENDAR_ID = 'charlie.king4967@gmail.com'
 
+
+
+async function getNextCalendarEvent(token) {
+    const response = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        params: {
+            maxResults: 1,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: 'startTime',
+        }
+    })
+
+    return response.data.items[0];
+}
 
 
 const createWindow = () => {
@@ -18,12 +39,12 @@ const createWindow = () => {
         client_id: CLIENT_ID,
         redirect_uri: REDIRECT_URI,
         response_type: 'code',
-        scope: 'https://www.googleapis.com/auth/calendar.events.readonly',
+        scope: SCOPES.join(' '),
         state: authState
     });
     authUrl.search = params;
 
-    win.webContents.on('will-redirect', (event, url) => {
+    win.webContents.on('will-redirect', async (event, url) => {
         const redirectUrl = new URL(url);
         if (redirectUrl.host === '127.0.0.1') {
             if (redirectUrl.searchParams.get('state') !== authState) {
@@ -32,7 +53,6 @@ const createWindow = () => {
             }
             event.preventDefault()
             const authCode = redirectUrl.searchParams.get('code');
-            console.log("CODE: ", authCode)
 
             const params = new URLSearchParams({
                 client_id: CLIENT_ID,
@@ -41,14 +61,20 @@ const createWindow = () => {
                 grant_type: 'authorization_code',
                 redirect_uri: 'http://127.0.0.1'
             });
-            axios.post('https://oauth2.googleapis.com/token', params.toString())
-            .then(res => {
-                const accessToken = res.data.access_token;
-                const refreshToken = res.data.refresh_token;
-                const expiresIn = res.data.expires_in;
-                console.log(accessToken, refreshToken, expiresIn);
-                win.close();
-            });
+            const requestTime = Math.floor(Date.now() / 1000);
+            const res = await axios.post('https://oauth2.googleapis.com/token', params.toString())
+            const accessToken = res.data.access_token;
+            const refreshToken = res.data.refresh_token;
+            const expiresIn = res.data.expires_in;
+            const expiresAt = requestTime + expiresIn;
+            const calEvent = await getNextCalendarEvent(accessToken)
+            
+            const { summary: name, start: { dateTime: time } } = calEvent;
+
+            console.log(name);
+            console.log(time);
+
+            win.close();
         }
     });
 
