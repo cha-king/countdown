0c76cc05257e82c7368ee56df243a4cecc928f4b
Charlie King <charlie.king@verkada.com>
2021-12-23 15:41:49 -0800

    Add config module

---
+++ config.js
@@ -0,0 +1,10 @@
+const CLIENT_ID = '339864928771-mtbpret2idljjjlsvto3h4uoglbfi0u9.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'http://127.0.0.1'
+const SCOPES = [
+    'https://www.googleapis.com/auth/calendar.events.readonly',
+];
+const CALENDAR_ID = 'charlie.king4967@gmail.com'
+
+
+module.exports = {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, CALENDAR_ID}
